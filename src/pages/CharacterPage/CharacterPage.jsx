bc75fbee43b6923d9a9289439f24af4ad5df6033
Marina Palter <murimolda@gmail.com>
2025-06-11 16:19:52 +0300

    add characters elements

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,17 +1,32 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import s from "./CharacterPage.module.css"
 
 export const CharacterPage = () => {
+  const [characters, setCharacters] = useState([])
+
   useEffect(() => {
     axios.get("https://rickandmortyapi.com/api/character").then((res) => {
-      console.log(res)
+      setCharacters(res.data.results)
     })
   }, [])
 
+
   return (
-    <div>
-      <h1 className = {`pageTitle`}>CharacterPage</h1>
+    <div className={"pageContainer"}>
+      <h1 className={"pageTitle"}>CharacterPage</h1>
+      {characters.length && (
+        <div className={s.characters}>
+          {characters.map((character) => {
+            return (
+              <div key={character.id} className={s.character}>
+                <div className={s.characterLink}>{character.name}</div>
+                <img src={character.image} alt={`${character.name} avatar`} />
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
